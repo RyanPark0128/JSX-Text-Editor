6ee7729b2640eef416d5cf0c4beb59dac5463b64
RyanPark0128 <vazywon@hotmail.com>
2021-02-05 12:24:34 -0800

    feat: add sandboxed iframe to build and evaluate code, and implement text editor

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,12 @@ import ReactDom from 'react-dom';
 import { useState, useEffect, useRef } from 'react';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import Editor from './components/Editor';
 
 const App = () => {
 	const ref = useRef<any>();
+	const iframe = useRef<any>();
 	const [input, setInput] = useState('');
-	const [code, setCode] = useState('');
 
 	useEffect(() => {
 		const startService = async () => {
@@ -24,6 +25,9 @@ const App = () => {
 		if (!ref.current) {
 			return;
 		}
+
+		iframe.current.srcdoc = html;
+
 		const result = await ref.current.build({
 			entryPoints: ['index.js'],
 			bundle: true,
@@ -34,13 +38,33 @@ const App = () => {
 				global: 'window'
 			}
 		});
-
-		// console.log(result);
-		setCode(result.outputFiles[0].text);
+		// setCode(result.outputFiles[0].text);
+		iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
 	};
+
+	const html = `
+		<html>
+			<head></head>
+			<body>
+				<div id="root"></div>
+				<script>
+					window.addEventListener('message', (event) => {
+						try {
+							eval(event.data);
+						} catch (err) {
+							const root = document.querySelector('#root');
+							root.innerHTML= '<div style="color: red"><h4>Error</h4>' + err + '</div>'
+							console.error(err);
+						}
+					}, false)
+				</script>
+			</body>
+		</html>
+	`;
 
 	return (
 		<div>
+			<Editor />
 			<textarea
 				value={input}
 				onChange={e => setInput(e.target.value)}
@@ -48,7 +72,12 @@ const App = () => {
 			<div>
 				<button onClick={onClick}>Submit</button>
 			</div>
-			<pre>{code}</pre>
+			<iframe
+				title="preview"
+				ref={iframe}
+				sandbox="allow-scripts"
+				srcDoc={html}
+			/>
 		</div>
 	);
 };
